1614dc106e0395f08bb071f7f94ad529031e8157
velgarde <deymriganka2004@gmail.com>
2023-06-16 16:03:30 +0530

    Solved second counter problem

---
+++ 02-async-js/easy/2-counter.js
@@ -0,0 +1,14 @@
+function count(n) {
+    let counter = 0;
+
+    function printCounter() {
+        console.clear();
+        console.log(counter);
+        counter++;
+        if (counter <= n) {
+            setTimeout(printCounter, 1000);
+        }
+    }
+    printCounter();
+}
+countUpToN(100);
